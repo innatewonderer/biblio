6047f2d5672caad831e37acdcaabb0dbc0a7bb6f
Jenya Zueva <jen6ka@Jenyas-MacBook-Air.local>
2013-01-25 15:37:02 -0500

    complete scrape of infinite scroll. database populated

--- db/brainpickings-scraper.rb
+++ db/brainpickings-scraper.rb
@@ -37,7 +37,7 @@ SQL
 
 #Scraping all pages
 
-1.upto(50) do |page_number|
+1.upto(3) do |page_number|
   books_url = "http://bookpickings.brainpickings.org/"
   books_url << 'page/' + page_number.to_s
   doc = Nokogiri::HTML(open(books_url))
@@ -46,20 +46,16 @@ SQL
 
   books.each do |book|
     # puts "=================="
-    title = book.at_css(".post_title h1").text
-    
-    if book.at_css(".post_title h2").nil?
-      author = 'unknown'
-    else
-      author = book.at_css(".post_title h2").text
-    end
-    
-    # puts author
+    title = book.at_css(".post_title h1").text  
+      if book.at_css(".post_title h2").nil?
+        author = 'unknown'
+      else
+        author = book.at_css(".post_title h2").text
+      end
     link = book.at_css("a.post_title").attr('href')
     description = book.at_css("a.post_title p").text
     img = book.at_css(".post_thumbnail a img").attr('src')
-
     db.execute("INSERT INTO books (title, author, link, description, img) 
-                  # VALUES (?, ?, ?, ?, ?)", title, author, link, description, img)
+                VALUES (?, ?, ?, ?, ?)", title, author, link, description, img)
   end
 end
